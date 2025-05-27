439997fd0d76aa7e3c752f5ab52e51fcf0ff6864
jalilovdavlatshoh49 <143313280+jalilovdavlatshoh49@users.noreply.github.com>
2025-05-27 09:50:14 +0500

    Delete uploads directory

--- uploads/src/middleware/errorHandler.ts
+++
@@ -1,8 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
-  console.error(err.stack);
-  res.status(err.status || 500).json({
-    error: err.message || "Internal Server Error",
-  });
-}
